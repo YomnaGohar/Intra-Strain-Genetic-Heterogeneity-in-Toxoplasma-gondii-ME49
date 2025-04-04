1e027df96e18b2809e201c8b894d41e48e49eebd
“YomnaGohar” <gohar.yomna@gmail.com>
2025-04-04 19:07:19 +0200

    find longest read

---
+++ script/find_longes_read_name.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  4 19:03:17 2025
+
+@author: yomna
+"""
+
+
+import pysam
+import sys
+bam_file = sys.argv[1]
+chromosome= sys.argv[2]
+start_pos = sys.argv[3]
+end_pos = sys.argv[4]
+samfile = pysam.AlignmentFile(bam_file, "rb")
+longest_read = []
+longest_length = 0
+for read in samfile.fetch(chromosome, start_pos, end_pos):
+
+    # Find the longest read
+    if read.query_length > longest_length: #and (read_start >= start_pos and read_end <= end_pos):
+        longest_length = read.query_length
+        longest_read = read
+samfile.close()
+
+print("longest_read", longest_read.query_name)
